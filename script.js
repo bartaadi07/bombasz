adb6190bb8494ec4b9b384784d137bb8d9fc2f3a
bartaadi07 <adam070702@gmail.com>
2025-03-04 21:07:55 +0100

    Add files via upload

--- script.js
+++ script.js
@@ -1,3 +1,4 @@
+// Navbar toggle
 const navSlide = () => {
     const burger = document.querySelector('.burger');
     const nav = document.querySelector('.nav-links');
@@ -16,6 +17,68 @@ const navSlide = () => {
 
         burger.classList.toggle('toggle');
     });
+};
+
+// Initialize
+navSlide();
+
+var isCtrl = false;
+document.onkeyup = function (e) {
+    if (e.which == 17)
+        isCtrl = false;
+}
+document.onkeydown = function (e) {
+    if (e.which == 123)
+        isCtrl = true;
+    if (((e.which == 85) || (e.which == 65) || (e.which == 88) || (e.which == 67) || (e.which == 86) || (e.which == 2) || (e.which == 3) || (e.which == 123) || (e.which == 83)) && isCtrl == true) {
+        alert('CopyRight SP!!!');
+        return false;
+    }
+}
+// right click code
+var isNS = (navigator.appName == "Netscape") ? 1 : 0;
+if (navigator.appName == "Netscape") document.captureEvents(Event.MOUSEDOWN || Event.MOUSEUP);
+function mischandler() {
+    alert('CopyRight SP!!!');
+    return false;
+}
+function mousehandler(e) {
+    var myevent = (isNS) ? e : event;
+    var eventbutton = (isNS) ? myevent.which : myevent.button;
+    if ((eventbutton == 2) || (eventbutton == 3)) return false;
+}
+document.oncontextmenu = mischandler;
+document.onmousedown = mousehandler;
+document.onmouseup = mousehandler;
+//select content code disable  alok goyal
+function killCopy(e) {
+    return false
+}
+function reEnable() {
+    return true
+}
+document.onselectstart = new Function("return false")
+if (window.sidebar) {
+    document.onmousedown = killCopy
+    document.onclick = reEnable
 }
 
-navSlide();
+// Disable right-click
+document.addEventListener('contextmenu', (e) => e.preventDefault());
+
+function ctrlShiftKey(e, keyCode) {
+    return e.ctrlKey && e.shiftKey && e.keyCode === keyCode.charCodeAt(0);
+}
+
+document.onkeydown = (e) => {
+    // Disable F12, Ctrl + Shift + I, Ctrl + Shift + J, Ctrl + U
+    if (
+        event.keyCode === 123 ||
+        ctrlShiftKey(e, 'I') ||
+        ctrlShiftKey(e, 'J') ||
+        ctrlShiftKey(e, 'C') ||
+        (e.ctrlKey && e.keyCode === 'U'.charCodeAt(0))
+    )
+        return false;
+};
+
